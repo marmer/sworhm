18d2b695b9e0402fae89a94a1a871ce1023cd06d
MarMer <1055445+marmer@users.noreply.github.com>
2018-11-18 00:33:46 +0100

    lots of values are getting provided

--- testdata-core/src/test/java/io/github/marmer/sworhm/testutils/testdata/IncrementalValueProviderTest.java
+++ testdata-core/src/test/java/io/github/marmer/sworhm/testutils/testdata/IncrementalValueProviderTest.java
@@ -18,8 +18,35 @@ class IncrementalValueProviderTest {
         return Stream.of(
                 arguments("nextLong",
                         new Object[]{1L, 2L, 3L},
-                        (Function<IncrementalValueProvider, Long>) IncrementalValueProvider::nextLong
-                ));
+                        (Function<IncrementalValueProvider, Long>) IncrementalValueProvider::nextLong)
+                , arguments("nextBoolean",
+                        new Object[]{true, false, true},
+                        (Function<IncrementalValueProvider, Boolean>) IncrementalValueProvider::nextBoolean)
+                , arguments("nextByte",
+                        new Object[]{(byte) 1, (byte) 2, (byte) 3},
+                        (Function<IncrementalValueProvider, Byte>) IncrementalValueProvider::nextByte)
+                , arguments("nextByte",
+                        new Object[]{(byte) 1, (byte) 2, (byte) 3},
+                        (Function<IncrementalValueProvider, Byte>) IncrementalValueProvider::nextByte)
+                , arguments("nextChar",
+                        new Object[]{(char) 1, (char) 2, (char) 3},
+                        (Function<IncrementalValueProvider, Character>) IncrementalValueProvider::nextChar)
+                , arguments("nextShort",
+                        new Object[]{(short) 1, (short) 2, (short) 3},
+                        (Function<IncrementalValueProvider, Short>) IncrementalValueProvider::nextShort)
+                , arguments("nextInt",
+                        new Object[]{1, 2, 3},
+                        (Function<IncrementalValueProvider, Integer>) IncrementalValueProvider::nextInt)
+                , arguments("nextFloat",
+                        new Object[]{1.2f, 2.3f, 3.4f},
+                        (Function<IncrementalValueProvider, Float>) IncrementalValueProvider::nextFloat)
+                , arguments("nextDouble",
+                        new Object[]{1.2d, 2.3d, 3.4d},
+                        (Function<IncrementalValueProvider, Double>) IncrementalValueProvider::nextDouble)
+                , arguments("nextString",
+                        new Object[]{"1", "2", "3"},
+                        (Function<IncrementalValueProvider, String>) IncrementalValueProvider::nextString)
+        );
     }
 
     @ParameterizedTest
