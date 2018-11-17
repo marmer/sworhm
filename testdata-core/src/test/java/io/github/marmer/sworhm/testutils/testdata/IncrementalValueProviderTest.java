4a5fa1c5aea66226df9e7daccaffe2bcea0b1c88
MarMer <1055445+marmer@users.noreply.github.com>
2018-11-18 00:08:22 +0100

    changed test to parameterized test

--- testdata-core/src/test/java/io/github/marmer/sworhm/testutils/testdata/IncrementalValueProviderTest.java
+++ testdata-core/src/test/java/io/github/marmer/sworhm/testutils/testdata/IncrementalValueProviderTest.java
@@ -1,28 +1,42 @@
 package io.github.marmer.sworhm.testutils.testdata;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
-import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class IncrementalValueProviderTest {
-    @Test
-    public void testNextLong_InitializedWithOneAndAnIncrementOf1_ShouldServeValuesWithAnIncrementOfOne()
+
+    private static Stream<Arguments> defaultIncrements() {
+        return Stream.of(
+                arguments("nextLong",
+                        new Object[]{1L, 2L, 3L},
+                        (Function<IncrementalValueProvider, Long>) IncrementalValueProvider::nextLong
+                ));
+    }
+
+    @ParameterizedTest
+    @MethodSource("defaultIncrements")
+    @DisplayName("Test with default increments and base")
+    public void defaultIncremtnTests(final String description, final Object[] expectedValues, final Function<IncrementalValueProvider, ?> method)
             throws Exception {
         // Preparation
         final IncrementalValueProvider underTest = new IncrementalValueProvider();
 
         // Execution
-        final List<Long> values = asList(
-                underTest.nextLong(),
-                underTest.nextLong(),
-                underTest.nextLong());
+        final Object[] values = Stream.generate(() -> method.apply(underTest))
+                .limit(expectedValues.length)
+                .toArray();
 
         // Assertion
-        assertThat(values, contains(1L, 2L, 3L));
+        assertThat(values, arrayContaining(expectedValues));
     }
 
 }
