67b880788d1e000e3d40709443cd1a5a9842b91a
MarMer <1055445+marmer@users.noreply.github.com>
2018-11-30 10:18:27 +0100

    application test integrated again and script for flyway added

---
+++ sworhm-server/src/test/java/io/github/marmer/sworhm/SworhmServerApplicationTest.java
@@ -0,0 +1,16 @@
+package io.github.marmer.sworhm;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class SworhmServerApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
