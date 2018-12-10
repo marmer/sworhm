b5b5f5d13a301f6898a83bb4941f54cf1c037719
marmer <Mariano.Mertinat@gmail.com>
2018-12-10 11:22:49 +0100

    Dummy persistance port implementation

---
+++ sworhm-server/src/main/java/io/github/marmer/sworhm/core/persistence/impl/DummyBookingPersistencePortImpl.java
@@ -0,0 +1,26 @@
+package io.github.marmer.sworhm.core.persistence.impl;
+
+import io.github.marmer.sworhm.core.model.Booking;
+import io.github.marmer.sworhm.core.persistence.BookingPersistencePort;
+
+import javax.inject.Named;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * TODO Replace me!
+ */
+@Named
+public class DummyBookingPersistencePortImpl implements BookingPersistencePort {
+    @Override
+    public Booking storeBooking(final Booking booking) {
+        return Booking.builder().startTime(LocalTime.now()).build();
+    }
+
+    @Override
+    public List<Booking> findBookingsByDay(final LocalDate day) {
+        return Collections.emptyList();
+    }
+}
