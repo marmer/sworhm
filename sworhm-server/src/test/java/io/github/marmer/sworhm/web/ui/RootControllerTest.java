218d6ca7b7348cd2248eded9d9ba87518f96661b
MarMer <1055445+marmer@users.noreply.github.com>
2018-12-10 18:20:47 +0100

    - Test for default redirection added

---
+++ sworhm-server/src/test/java/io/github/marmer/sworhm/web/ui/RootControllerTest.java
@@ -0,0 +1,37 @@
+package io.github.marmer.sworhm.web.ui;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+class RootControllerTest {
+    @Autowired
+    private RootController underTest;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Rootcall should redirect to bookings default page")
+    public void testGetRoot_RootcallShouldRedirectToBookingsDefaultPage()
+            throws Exception {
+        // Preparation
+
+        // Execution
+        final ResultActions result = mockMvc.perform(get("/"));
+
+        // Assertion
+        result.andExpect(status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrlTemplate("/bookings"));
+    }
+}
