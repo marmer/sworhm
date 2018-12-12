804d28a620fb107ed1b8ea1c9187b7f944e3c8a4
marmer <mariano.mertinat@gmail.com>
2018-12-13 00:22:24 +0100

    small steps to a layout

--- sworhm-server/src/main/java/io/github/marmer/sworhm/web/ui/BookingController.java
+++ sworhm-server/src/main/java/io/github/marmer/sworhm/web/ui/BookingController.java
@@ -1,13 +1,18 @@
 package io.github.marmer.sworhm.web.ui;
 
 import io.github.marmer.sworhm.core.SystemTimeService;
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @Controller
 @RequestMapping
@@ -29,8 +34,16 @@ public class BookingController {
     }
 
     @GetMapping("/day/:{day}/bookings")
-    String getBookingsForDay(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate day) {
+    ModelAndView getBookingsForDay(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate day) {
         // TODO: marmer 10.12.2018 you know what todo ;)
-        return "bookings";
+        final List<BookingDTO> bookings = asList(new BookingDTO().setDescription("first"),
+                new BookingDTO().setDescription("second"),
+                new BookingDTO().setDescription("third"));
+        return new ModelAndView("bookings", "bookings", bookings);
+    }
+
+    @Data
+    public static class BookingDTO {
+        private String description;
     }
 }
