baeeaa9a5f5b803095fb270ab6b4dbe4b9e388f8
MarMer <1055445+marmer@users.noreply.github.com>
2019-01-13 11:54:35 +0100

    post mapping spike

--- sworhm-server/src/main/java/io/github/marmer/sworhm/web/ui/dto/BookingDTO.java
+++ sworhm-server/src/main/java/io/github/marmer/sworhm/web/ui/dto/BookingDTO.java
@@ -1,6 +1,7 @@
 package io.github.marmer.sworhm.web.ui.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -8,8 +9,11 @@ import java.time.LocalTime;
 @Data
 public class BookingDTO {
     private String id;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate day;
+    @DateTimeFormat(pattern = "HH:mm")
     private LocalTime startTime;
+    @DateTimeFormat(pattern = "HH:mm")
     private LocalTime endTime;
     private LocalTime duration;
     private String notes;
