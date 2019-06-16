4ad7784a84c9872d58f841b3b26d46521e7deecc
MarMer <1055445+marmer@users.noreply.github.com>
2019-06-16 14:49:10 +0200

    DTO Model introduced

--- sworhm-server/src/main/java/io/github/marmer/sworhm/rest/v1/BookingController.java
+++ sworhm-server/src/main/java/io/github/marmer/sworhm/rest/v1/BookingController.java
@@ -1,47 +1,55 @@
 package io.github.marmer.sworhm.rest.v1;
 
-import lombok.Value;
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/days/{bookingDay}/bookings")
 public class BookingController {
     @GetMapping
-    public ResponseEntity<String> getBookings(
+    public BookingsDto getBookings(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate bookingDay) {
-        return ResponseEntity.status(200)
-                .body("{\n" +
-                        "  \"day\": \"2002-02-01\",\n" +
-                        "  \"entries\": [\n" +
-                        "    {\n" +
-                        "      \"id\": \"65b8818f-0320-450b-9da0-49f3269bafd7\",\n" +
-                        "      \"startTime\": \"00:55\",\n" +
-                        "      \"durationInMinutes\": 45,\n" +
-                        "      \"description\": \"another one bites the dust\",\n" +
-                        "      \"ticket\": \"JIRA-666\",\n" +
-                        "      \"notes\": \"knocking on heavens door\"\n" +
-                        "    },\n" +
-                        "    {\n" +
-                        "      \"id\": \"1412c9ec-4abe-44d2-a8af-406c45a55b54\",\n" +
-                        "      \"startTime\": \"01:55\",\n" +
-                        "      \"durationInMinutes\": 117,\n" +
-                        "      \"description\": \"stay alive\",\n" +
-                        "      \"ticket\": \"JIRA-999\",\n" +
-                        "      \"notes\": \"cheek to cheek\"\n" +
-                        "    }\n" +
-                        "  ]\n" +
-                        "}\n");
+        return new BookingsDto()
+                .setBookingDay("2002-02-01")
+                .setBookings(List.of(
+                        new BookingDto()
+                                .setId("65b8818f-0320-450b-9da0-49f3269bafd7")
+                                .setStartTime("00:55")
+                                .setDurationInMinutes(45)
+                                .setDecscription("another one bites the dust")
+                                .setTicket("JIRA-666")
+                                .setNotes("knocking on heavens door"),
+                        new BookingDto()
+                                .setId("1412c9ec-4abe-44d2-a8af-406c45a55b54")
+                                .setStartTime("01:55")
+                                .setDurationInMinutes(117)
+                                .setDecscription("stay alive")
+                                .setTicket("JIRA-999")
+                                .setNotes("cheek to cheek")
+                ));
     }
 
-    @Value
+    @Data
     public static class BookingsDto {
-        private final String bookingDay;
+        private String bookingDay;
+        private List<BookingDto> bookings = new ArrayList<>();
+    }
+
+    @Data
+    private static class BookingDto {
+        private String id;
+        private String startTime;
+        private int durationInMinutes;
+        private String decscription;
+        private String ticket;
+        private String notes;
     }
 }
