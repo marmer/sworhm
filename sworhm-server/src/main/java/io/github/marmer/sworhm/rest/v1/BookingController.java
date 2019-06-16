199c4b5485e93654a17dac50d6b08fe638a97b35
MarMer <1055445+marmer@users.noreply.github.com>
2019-06-16 14:57:31 +0200

    some date formatting

--- sworhm-server/src/main/java/io/github/marmer/sworhm/rest/v1/BookingController.java
+++ sworhm-server/src/main/java/io/github/marmer/sworhm/rest/v1/BookingController.java
@@ -1,5 +1,6 @@
 package io.github.marmer.sworhm.rest.v1;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,18 +20,18 @@ public class BookingController {
     public BookingsDto getBookings(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate bookingDay) {
         return new BookingsDto()
-                .setBookingDay("2002-02-01")
+                .setBookingDay(bookingDay)
                 .setBookings(List.of(
                         new BookingDto()
                                 .setId("65b8818f-0320-450b-9da0-49f3269bafd7")
-                                .setStartTime("00:55")
+                                .setStartTime(LocalTime.of(0, 55))
                                 .setDurationInMinutes(45)
                                 .setDescription("another one bites the dust")
                                 .setTicket("JIRA-666")
                                 .setNotes("knocking on heavens door"),
                         new BookingDto()
                                 .setId("1412c9ec-4abe-44d2-a8af-406c45a55b54")
-                                .setStartTime("01:55")
+                                .setStartTime(LocalTime.of(1, 53))
                                 .setDurationInMinutes(117)
                                 .setDescription("stay alive")
                                 .setTicket("JIRA-999")
@@ -37,15 +39,16 @@ public class BookingController {
     }
 
     @Data
-    public static class BookingsDto {
-        private String bookingDay;
+    private static class BookingsDto {
+        private LocalDate bookingDay;
         private List<BookingDto> bookings = new ArrayList<>();
     }
 
     @Data
     private static class BookingDto {
         private String id;
-        private String startTime;
+        @JsonFormat(pattern = "HH:mm")
+        private LocalTime startTime;
         private int durationInMinutes;
         private String description;
         private String ticket;
