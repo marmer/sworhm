c2dcfb6418e6d19e1de73994157402a9d2f67ebf
MarMer <1055445+marmer@users.noreply.github.com>
2019-06-21 01:20:40 +0200

    not need for an h2 db anymore

--- sworhm-server/src/test/java/io/github/marmer/sworhm/SworhmServerApplicationTest.java
+++ sworhm-server/src/test/java/io/github/marmer/sworhm/SworhmServerApplicationTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest({"spring.jpa.hibernate.ddl-auto=create-drop", "spring.datasource.url=jdbc:h2:mem:sworhm", "spring.jpa.database=h2"})
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
 class SworhmServerApplicationTest {
 
