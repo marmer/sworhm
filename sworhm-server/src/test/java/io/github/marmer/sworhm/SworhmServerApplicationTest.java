a9d32920a396607d6af5683356b0cd1739ff6bad
MarMer <1055445+marmer@users.noreply.github.com>
2019-06-21 01:19:40 +0200

    DB-Port is configurable now by a maven property

--- sworhm-server/src/test/java/io/github/marmer/sworhm/SworhmServerApplicationTest.java
+++ sworhm-server/src/test/java/io/github/marmer/sworhm/SworhmServerApplicationTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest({"spring.jpa.hibernate.ddl-auto=create-drop", "spring.datasource.url=jdbc:h2:mem:sworhm"})
+@SpringBootTest({"spring.jpa.hibernate.ddl-auto=create-drop", "spring.datasource.url=jdbc:h2:mem:sworhm", "spring.jpa.database=h2"})
 @ExtendWith(SpringExtension.class)
-public class SworhmServerApplicationTest {
-
+class SworhmServerApplicationTest {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
+        //context is able to load
     }
 
 }
