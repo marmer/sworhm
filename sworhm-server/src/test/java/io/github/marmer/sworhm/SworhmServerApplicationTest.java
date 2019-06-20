604e52acabed2f5118d939e7298335746c7cbafd
MarMer <1055445+marmer@users.noreply.github.com>
2019-06-20 22:07:17 +0200

    docker db for integration tests with maven

--- sworhm-server/src/test/java/io/github/marmer/sworhm/SworhmServerApplicationTest.java
+++ sworhm-server/src/test/java/io/github/marmer/sworhm/SworhmServerApplicationTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@SpringBootTest({"spring.jpa.hibernate.ddl-auto=create-drop", "spring.datasource.url=jdbc:h2:mem:sworhm"})
 @ExtendWith(SpringExtension.class)
 public class SworhmServerApplicationTest {
 
