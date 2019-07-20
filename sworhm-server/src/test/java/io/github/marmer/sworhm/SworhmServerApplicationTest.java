2f8831e7ed05786f333991b07856c2492b27bb05
MarMer <1055445+marmer@users.noreply.github.com>
2019-07-20 09:07:23 +0200

    refactoring

--- sworhm-server/src/test/java/io/github/marmer/sworhm/SworhmServerApplicationTest.java
+++ sworhm-server/src/test/java/io/github/marmer/sworhm/SworhmServerApplicationTest.java
@@ -3,9 +3,11 @@ package io.github.marmer.sworhm;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest({"spring.jpa.hibernate.ddl-auto=create-drop", "spring.datasource.url=jdbc:h2:mem:sworhm", "spring.jpa.database=h2"})
+@SpringBootTest
+@ActiveProfiles("inMemoryDb")
 @ExtendWith(SpringExtension.class)
 class SworhmServerApplicationTest {
 
